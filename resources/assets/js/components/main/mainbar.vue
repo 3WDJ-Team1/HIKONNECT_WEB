1b2e4876468bf5b9f5a1c2caddee59c4aff89993
Choijungyu <wnsrb0147@naver.com>
2018-03-07 22:57:55 +0900

    메인페이지0.2v

--- resources/assets/js/components/main/mainbar.vue
+++ resources/assets/js/components/main/mainbar.vue
@@ -14,10 +14,6 @@
                         <li><router-link :to="{ name : '#'}">알림</router-link></li>
                         <li ><router-link :to="{ name : '#'}">마이페이지</router-link></li>
                     </ul>
-                    <div style="position: absolute; margin-left: 700px">
-                    <p v-if="sessionver() == 'true'"><button v-on:click ="logout">로그아웃 </button> </p>
-                    <p v-else> <router-link :to ="{ name: 'login'}"><button> 로그인</button></router-link></p>
-                    </div>
                 </div>
             </div>
         </nav>
@@ -25,17 +21,6 @@
 </template>
 <script>
     export default {
-            methods: {
-                sessionver() {
-                    if(sessionStorage.getItem('login') != null) {
-                        return 'true';
-                    }
-                },
-                logout() {
-                    sessionStorage.clear();
-                    this.$router.push({ name: 'main'});
-                    window.location.reload();
-                }
-            }
+
     }
 </script>
--- resources/assets/js/components/main/test.vue
+++ resources/assets/js/components/main/test.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-
+        <header id="header">
+            <h1><a href="index.html">Transit</a></h1>
+            <nav id="nav">
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="generic.html">Generic</a></li>
+                    <li><a href="elements.html">Elements</a></li>
+                    <li><a href="#" class="button special">Sign Up</a></li>
+                </ul>
+            </nav>
+        </header>
     </div>
 </template>
 <script>
