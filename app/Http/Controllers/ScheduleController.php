fa7876a3d3e1a80d73ff4788e41faae5344c43f2
SongSol <thdthf159@naver.com>
2018-05-03 01:25:24 +0900

    Schedule Regist

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -60,7 +60,6 @@ class ScheduleController extends Controller
             'mnt_id'        => 491303004
         ]);
         $this->hiking_schedule->scheduleReg($info);
-
         return response()->json('true');
     }
 
