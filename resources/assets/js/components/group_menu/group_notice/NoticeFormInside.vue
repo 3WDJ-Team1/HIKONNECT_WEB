e3fe512aaed37a929e355375bf1767564f279bbf
jiyoon <di03154@naver.com>
2018-05-02 21:00:44 +0900

    dooo

--- resources/assets/js/components/group_menu/group_notice/NoticeFormInside.vue
+++ resources/assets/js/components/group_menu/group_notice/NoticeFormInside.vue
@@ -12,27 +12,27 @@
             <v-subheader style="font-size: 20px;">title</v-subheader>
             <!-- @v-text-field#notice_input_title       text of title will be input -->
             <v-text-field
-                name        ="notice_title"
-                v-model     ="title"
-                :rules      ="titleRules"
-                required
+                    name="notice_title"
+                    v-model="title"
+                    :rules="titleRules"
+                    required
             ></v-text-field>
             <br>
             <v-subheader style="font-size: 20px;">text</v-subheader>
             <!-- @v-text-field#notice_input_textarea    text of content will be input -->
             <v-text-field
-                name        ="notice_text"
-                v-model     ="text"
-                :rules      ="textRules"
-                textarea
-                rows        ="12"
-                required
+                    name="notice_text"
+                    v-model="text"
+                    :rules="textRules"
+                    textarea
+                    rows="12"
+                    required
             ></v-text-field>
             <!-- v-btn      button for submit -->
             <v-btn
-                @click      ="submit"
-                style       ="height: 100%; color: white;"
-                color       ="cyan"
+                    @click="submit"
+                    style="height: 100%; color: white;"
+                    color="cyan"
             >
                 submit
             </v-btn>
@@ -42,7 +42,7 @@
 
 <script>
     export default {
-        data : () => ({
+        data: () => ({
             /**
              * title        (String)    the title of a notice which was selected.
              *                          if mode is 'edit', it will get value from event bus.
@@ -55,17 +55,17 @@
              * textRules    (Array)     array of functions about rules of content. (not null)
              * mode         (String)    the mode of modal (edit or write)
              */
-            title       : '',
-            text        : '',
-            noticeUuid  : '',
-            valid       : false,
-            titleRules  : [
+            title: '',
+            text: '',
+            noticeUuid: '',
+            valid: false,
+            titleRules: [
                 title => !!title || 'Title is required.'
             ],
-            textRules   : [
+            textRules: [
                 text => !!text || 'Text is required.'
             ],
-            mode        : '',
+            mode: '',
         }),
         methods: {
             /**
@@ -73,12 +73,12 @@
              * @brief       if submit button is clicked, send http request.
              */
             submit() {
-                axios.post(this.$HttpAddr + '/api/notice', {
-                    writer  : sessionStorage.uuid, // user's uuid,
-                    title   : this.title,
-                    content : this.text
-                }).then((response)=> {
-
+                axios.post(this.$HttpAddr + '/notice', {
+                    // nickname: this.nickname,
+                    writer: sessionStorage.getItem('userid'), // user's uuid,
+                    uuid: this.$route.params.groupid,
+                    title: this.title,
+                    content: this.text
                 });
                 this.$parent.close();
                 this.$EventBus.$emit('newNoticeWrited', true);
@@ -91,15 +91,15 @@
          */
         created() {
             this.$EventBus.$on('noticeData', (data) => {
-                this.title      = data.title;
-                this.text       = data.content;
+                this.title = data.title;
+                this.text = data.content;
                 this.noticeUuid = data.uuid;
             });
             this.$EventBus.$on('modalMode', (modalMode) => {
-                this.mode       = modalMode;
+                this.mode = modalMode;
             });
         }
-            
+
     }
 </script>
 
@@ -111,6 +111,7 @@
     #notice_input_textarea {
         /* border: 1px solid gray; */
     }
+
     #notice_input_textarea:focus {
         /* border: 2px solid darkslategrey; */
     }
