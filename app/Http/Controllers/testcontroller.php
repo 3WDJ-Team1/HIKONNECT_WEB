8a378ad38941917e77f9a1246ed31a87df2e8b85
SongSol <thdthf159@naver.com>
2018-05-17 20:06:42 +0900

    testcontroller commit

--- app/Http/Controllers/testcontroller.php
+++ app/Http/Controllers/testcontroller.php
@@ -1,26 +1,40 @@
 <?php
-
+/**
+ * Test controller
+ *
+ * PHP version 7.0
+ *
+ * @category Controller
+ * @package  App\Http\Controllers
+ * @author   SongSol <address@email.com>
+ * @author   bs Kwon <rnjs9957@gmail.com>
+ * @license  MIT License
+ * @link     n
+ */
 namespace App\Http\Controllers;
-
-use App\Models\entry_info;
-use App\Models\LocationMemo;
-use App\Models\test;
 use DB;
-// use App\User;
-// use App\User_Profile;
+use App\Models\entry_info;
+use App\Models\location_memo;
+use App\Models\test;
+use App\Models\User;
+use App\Models\User_Profile;
+use App\Models\schedule_member;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+/**
+ * Test controller
+ *
+ * @category Controller
+ * @package  App\Http\Controller
+ * @author   bs Kwon <rnjs9957@gmail.com>
+ * @author   SongSol <address@domain.com>
+ * @license  MIT License
+ * @link     link
+ */
 class testcontroller extends Controller
 {
-
-    private $location_memo;
-
-    public function __construct()
-    {
-        $this->location_memo = new LocationMemo();
-    }
-
     /**
      * Display a listing of the resource.
      *
@@ -28,7 +42,7 @@ class testcontroller extends Controller
      */
     public function index()
     {
-        return "ㅎㅇㅎㅇ";
+        //
     }
     /**
      * Show the form for creating a new resource.
@@ -42,36 +56,45 @@ class testcontroller extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         $user_id    = $request->get('user_id');
-
-        $uuid = DB::table('user')->where('id',$user_id)->select('uuid')->get()[0]->uuid;
-        $hiking_group = DB::table('entry_info')->where('user',$uuid)->select('hiking_group')->get()[0]->hiking_group;
-
-        DB::table('location_memo')->insert([
-            'uuid'          => '',
-            'writer'        => $uuid,
-            'hiking_group'  => $hiking_group,
-            'latitude'      => $request->get('lat'),
-            'longitude'     => $request->get('lng'),
-            'title'         => $request->get('title'),
-            'content'       => $request->get('content'),
-            'image_path'    => $request->get('image_path'),
-            'created_at'    => $request->get('created_at'),
-            'updated_at'    => $request->get('updated_at')
-        ]);
-        
-        echo $uuid;
+        $latitude   = $request->get('lat');
+        $longitude  = $request->get('lng');
+        $title      = $request->get('title');
+        $content    = $request->get('content');
+        $image_path = $request->get('image_path');
+        $created_at = $request->get('created_at');
+        $updated_at = $request->get('updated_at');
+        $schedule_member = DB::table('schedule_member')
+            ->select('schedule', 'hiking_group')
+            ->where('userid', $user_id)
+            ->get();
+        $queryRes = DB::table('location_memo')
+            ->insert(
+                [
+                    'schedule_no'   => $schedule_member->get(0)->schedule,
+                    'hiking_group'  => $schedule_member->get(0)->hiking_group,
+                    'title'         => $title,
+                    'content'       => $content,
+                    'writer'        => $user_id,
+                    'picture'       => $image_path,
+                    'created_at'    => $created_at,
+                    'updated_at'    => $updated_at,
+                    'latitude'      => $latitude,
+                    'longitude'     => $longitude
+                ]
+            );
     }
-
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -81,7 +104,8 @@ class testcontroller extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -91,8 +115,9 @@ class testcontroller extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int                      $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -102,75 +127,21 @@ class testcontroller extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         //
     }
-    
-    /**
-     * Store location memo.
-     * 
-     * @param Request $request .
-     * 
-     * @var String $user_id     User ID.
-     * @var String $latitude    User latitude.
-     * @var String $longitude   User longitude.
-     * @var String $title       Memo title.
-     * @var String $content     Memo contents.
-     * @var String $image_path  Memo image file path.
-     * @var String $created_at  Created time.
-     * @var String $updated_at  Updated time.
-     * 
-     * @return ??
-     */
-    public function storeLocationMemo(Request $request)
-    {
-        $member_no  = $request->get('member_no');
-        $latitude   = $request->get('lat');
-        $longitude  = $request->get('lng');
-        $title      = $request->get('title');
-        $content    = $request->get('content');
-        $image_path = $request->get('image_path');
-        $created_at = date("Y-m-d H:i:s");
-        $updated_at = date("Y-m-d H:i:s");
-
-        $schedule_member = DB::table('schedule_member')
-            ->select(
-                'schedule', 
-                'hiking_group', 
-                'userid'
-            )->where('member_no', $member_no)
-            ->get();
-
-        $queryRes = DB::table('location_memo')
-        ->insert(
-            [
-                'schedule_no'   => $schedule_member->get(0)->schedule,
-                'hiking_group'  => $schedule_member->get(0)->hiking_group,
-                'title'         => $title,
-                'content'       => $content,
-                'writer'        => $schedule_member->get(0)->userid,
-                'picture'       => $image_path,
-                'created_at'    => $created_at,
-                'updated_at'    => $updated_at,
-                'latitude'      => $latitude,
-                'longitude'     => $longitude
-            ]
-        );
-
-        return $queryRes ? 'ture' : 'false';
-    }
-
     /**
      * Get detail information of location memo.
-     * 
+     *
      * @param Request $request .
-     * 
+     *
      * @var Integer $location_no [POST] location no.
-     * 
+     *
      * @return Array Detail information of location memo.
      *               [
      *                  title,          Memo's title.
@@ -179,10 +150,9 @@ class testcontroller extends Controller
      *                  picture        Stored picture directory path.
      *               ]
      */
-    public function getLocationMemoDetail(Request $request) 
+    public function getLocationMemoDetail(Request $request)
     {
         $location_no = $request->get('location_no');
-
         $queryRes = DB::select(
             DB::raw(
                 "SELECT
@@ -194,24 +164,22 @@ class testcontroller extends Controller
                 WHERE no = ${location_no};"
             )
         );
-
         return $queryRes;
     }
-
     /**
      * Initialize or update data of user paricapating in schedule.
-     * 
+     *
      * @param Request $request .
-     * 
+     *
      * @var Integer $user_id [POST] User's ID.
      * @var Double  $lat     [POST] User's latitude.
      * @var Double  $lng     [POST] User's longitude.
-     * 
+     *
      * @return JSON {
      *                  "members": {
-     *                      member_no, 
-     *                      latitude, 
-     *                      longitude, 
+     *                      member_no,
+     *                      latitude,
+     *                      longitude,
      *                  },
      *                  "location_memos": {
      *                      no,
@@ -227,9 +195,7 @@ class testcontroller extends Controller
         $lng            = $request->get('longitude');
         $distance       = $request->get('distance');
         $avg_speed      = $request->get('velocity');
-
         $arr_for_update = array();
-
         if ($lat && $lng) {
             $arr_for_update['latitude'] = $lat;
             $arr_for_update['longitude'] = $lng;
@@ -240,21 +206,17 @@ class testcontroller extends Controller
         if ($avg_speed) {
             $arr_for_update['avg_speed'] = $avg_speed;
         }
-
         $user_id_check = schedule_member::where('member_no', $member_no)
             ->get();
-
         schedule_member::where('member_no', $member_no)
-        ->update($arr_for_update);
-
+            ->update($arr_for_update);
         // Get the locations of members who participate in the same schedule.
         $pos_members = DB::select(
             DB::raw(
                 "SELECT
                     member_no,
                     latitude,
-                    longitude,
-                    userid
+                    longitude
                 FROM schedule_member
                 WHERE schedule = (
                     SELECT schedule
@@ -263,7 +225,6 @@ class testcontroller extends Controller
                 );"
             )
         );
-
         $pos_location_memo = DB::select(
             DB::raw(
                 "SELECT
@@ -278,24 +239,22 @@ class testcontroller extends Controller
                 )"
             )
         );
-        
+
         $httpResponse = [
             'members'           => $pos_members,
             'location_memos'    => $pos_location_memo
         ];
-
         return $httpResponse;
     }
-
     /**
      * Get current FID(Field ID) from data server(hikonnect.ga:3000).
-     * 
+     *
      * @param Integer $mntCode code which key of target mountain.
-     * @param Array   $fidSet  Field ID set which user has been 
+     * @param Array   $fidSet  Field ID set which user has been
      *                         participating schedule.
      * @param Double  $lat     User's latitude.
      * @param Double  $lng     User's longitude.
-     * 
+     *
      * @return String Field ID which user was located in specific mountain.
      */
     function getCurrentFID($mntCode, $fidSet, $lat, $lng)
@@ -303,11 +262,10 @@ class testcontroller extends Controller
         $mntCode    = intval($mntCode);
         $lat        = doubleval($lat);
         $lng        = doubleval($lng);
-
         try {
             $client = new Client(); // GuzzleHttp\Client
             $result = $client->get(
-                '172.26.2.38:3000/mountain/getCurrentFID', 
+                '172.26.2.38:3000/mountain/getCurrentFID',
                 [
                     'query' => [
                         'mntCode'   => $mntCode,
@@ -320,16 +278,135 @@ class testcontroller extends Controller
         } catch (GuzzleException $e) {
             return "Http Error occured.";
         }
-
-        // var_dump($post_data);
-
-        echo json_encode($post_data);
+        return $result->getBody()->getContents();
     }
-
-    public function send_image_path(Request $request) {
-        $path = location_memo::where('latitude',$request->get('lat'))->
-        where('longitude',$request->get('lng'))->get();
-
-        print json_encode($path);
+    /**
+     * Update user's hiking status.
+     *
+     * @param Request $request
+     *
+     * @var Integer $member_no [POST] Reference Key which participating user.
+     * @var Integer $state     [POST] Status flag.
+     *                             (0: End hiking, 1: Start hiking, 2: Start rescue).
+     *
+     * @return String true/false
+     */
+    function updateHikingState(Request $request)
+    {
+        $member_no  = $request->get('member_no');
+        $state      = $request->get('state');
+        $insertSet = [
+            'hiking_state' => $state
+        ];
+        if ($state == 1) {
+            $insertSet['hiking_start'] = date('Y-m-d H:i:s');
+        }
+        DB::table('schedule_member')
+            ->where('member_no', $member_no)
+            ->update($insertSet);
+    }
+    /**
+     * Get member list which user is participating in schedule.
+     *
+     * @param Request $request .
+     *
+     * @var Integer $member_no [POST] Unique number of the user.
+     *
+     * @return Array [
+     *                  member_no,  The Unique key of member who is
+     *                              participating in schedule.
+     *                  nickname,   The members' nickname.
+     *                  rank        The ranking which measure by distance
+     *                              of hiking from starting point.
+     *               ]
+     */
+    function getScheduleMembers(Request $request)
+    {
+        $member_no = $request->get('member_no');
+        $queryRes = DB::select(
+            DB::raw(
+                "SELECT 
+                    member_no,
+                    nickname,
+                    distance,
+                    (
+                        SELECT count(*) + 1
+                        FROM schedule_member
+                        WHERE distance > sm.distance
+                        AND schedule = (
+                            SELECT schedule
+                            FROM schedule_member
+                            WHERE member_no = '${member_no}'
+                        )
+                    ) as rank
+                FROM schedule_member as sm
+                JOIN user as u
+                ON u.userid = sm.userid
+                WHERE sm.schedule = (
+                    SELECT schedule
+                    FROM schedule_member as sub_sm
+                    WHERE member_no = '${member_no}'
+                )
+                AND sm.distance IS NOT NULL
+                ORDER BY rank;"
+            )
+        );
+        return $queryRes;
+    }
+    /**
+     * Get detail information of the member.
+     *
+     * @param Request $request .
+     *
+     * @var String $member_no           [POST] Member's unique number
+     *                                  who is participating in schedule.
+     *
+     * @return Array [
+     *                  nickname,       Nickname of the member.
+     *                  profile,        Member's profile image.
+     *                  distance,       The distance which member hiked.
+     *                  hiking_start,   Started time, member was started hiking.
+     *                  avg_speed,      Member's average hiking speed.
+     *                  rank            The ranking which measure by distance
+     *                                  of hiking from starting point.
+     *               ]
+     */
+    public function getMemberDetail(Request $request)
+    {
+        $member_no = $request->get('member_no');
+        $queryRes = DB::select(
+            DB::raw(
+                "SELECT
+                    u.nickname          as nickname,
+                    u.profile           as profile,
+                    hg.title            as hiking_group,
+                    sm.distance         as distance,
+                    sm.hiking_start     as hiking_start,
+                    sm.avg_speed        as velocity,
+                    (
+                        SELECT count(*) + 1
+                        FROM schedule_member
+                        WHERE distance > sm.distance
+                        AND schedule = sm.schedule
+                    ) as rank
+                FROM user as u
+                JOIN schedule_member as sm
+                ON u.userid = sm.userid
+                JOIN hiking_group as hg
+                ON hg.uuid = sm.hiking_group
+                WHERE sm.member_no = '${member_no}'
+                ORDER BY sm.distance;"
+            )
+        );
+        return $queryRes;
+    }
+    public function getMemberNo(Request $request)
+    {
+        $user_id = $request->get('user_id');
+        $queryRes = DB::table('schedule_member')
+            ->select('member_no')
+            ->where('userid', $user_id)
+            ->get();
+        return $queryRes;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,17 @@ Route::middleware(
         return $request->user();
     }
 );
+
+Route::get(
+    'testing/{key?}',
+    function ($mnt_name) {
+        return  DB::table('mountain')
+            ->where('mnt_name', 'LIKE', "%" . $mnt_name . "%")
+            ->get();
+    }
+);
+
+
 Route::group(
     [],
     function () {
@@ -169,10 +180,6 @@ Route::group(
             '/send',
             'FCMController@pushNotification'
         )->name('SendNotification');
-        Route::post(
-            '/storesend',
-            'testcontroller@store_send'
-        )->name('login_app');
         //Group Schedule
         Route::resource('/schedule',
             'ScheduleController'
@@ -221,6 +228,186 @@ Route::group(
             'hiking_count/{userid}'
             ,'ScheduleController@hiking_count'
         )->name('Count of hiking');
+
+        // Radios Routing
+        Route::get(
+            'radioGram/{groupId}',
+            'RadioGramContoller@getGroupRadios'
+        )->name('radios');
+        // Notification Routings
+        Route::resource(
+            'notice',
+            'NoticeController'
+        );
+        Route::get(
+            'notice/{groupUuid}/{pageIndex?}/{perPage?}',
+            'NoticeController@index'
+        )->name('noticePagination');
+        Route::delete(
+            'notice',
+            'NoticeController@destroy'
+        )->name('deleteNotice');
+
+        // Hiking group Routings
+        Route::resource(
+            'hikingGroup',
+            'HikingGroupController'
+        );
+        Route::get(
+            'groupList/{idx}/{perIdx}/{mntName}/{writer}/{date}',
+            'HikingGroupController@getGroupList'
+        )->name('groupList');
+        Route::get(
+            'isOwner/{groupId}/{userId}',
+            'HikingGroupController@isOwner'
+        )->name('isOwner');
+
+        // Entry info Routings
+        Route::post(
+            'entryGroup',
+            'EntryInfoController@entryGroup'
+        )->name('entryGroup');
+        Route::patch(
+            'replyUserEntry',
+            'EntryInfoController@replyUserEntry'
+        )->name('replyUserEntry');
+        Route::get(
+            'appliedUsers/{groupUuid}',
+            'EntryInfoController@appliedUsers'
+        )->name('appliedUsers');
+        Route::delete(
+            'rejectUserEntry',
+            'EntryInfoController@rejectUserEntry'
+        )->name('rejectUserEntry');
+
+        Route::get(
+            'groupMembers/{groupUuid}/{idx?}/{perIdx?}',
+            'HikingGroupController@getGroupMembers'
+        )->name('groupMemberList');
+
+        // User Profile Routings
+        Route::get(
+            'userProfile/{userUuid}',
+            'UserProfileController@getUserProfile'
+        );
+
+        // Login Routings
+        Route::resource(
+            '/user',
+            'UserController'
+        );
+        Route::post(
+            '/login',
+            'LoginController@login'
+        )->name('login');
+        Route::post(
+            '/loginprocess',
+            'LoginController@loginprocess'
+        )->name('loginprocess');
+        Route::post(
+            '/login_app',
+            'LoginController@login_app'
+        )->name('login_app');
+        /**
+         * Login process using Socialite
+         *
+         * Line     = enabled
+         * Kakao    = disabled
+         */
+        Route::get(
+            '/login/{providerName}',
+            'LoginController@redirectToProvider'
+        )->name('SNSLogin');
+        Route::get(
+            '/login/{providerName}/redirect',
+            'LoginController@handleProviderCallback'
+        )->name('SNSLoginRedirect');
+
+        Route::post(
+            '/logout',
+            'LoginController@logout'
+        )->name('logout');
+        Route::get(
+            '/user/{id}',
+            'UserController@getImage'
+        )->name('getImage');
+        Route::get(
+            '/mypage/{id}',
+            'UserController@showUserData'
+        )->name('UserData');
+        Route::post(
+            '/graph/{id}',
+            'UserController@graph'
+        )->name('graph');
+        Route::get(
+            'main/{id}',
+            'MainController@get_Announce_Count'
+        )->name('Announce_Count');
+
+        // 위치 메모 테스팅
+        Route::resource(
+            '/test',
+            'testcontroller'
+        );
+        Route::post(
+            '/send',
+            'FCMController@pushNotification'
+        )->name('SendNotification');
+        Route::post(
+            '/position',
+            'testcontroller@send_image_path'
+        )->name('sendimage');
+        // 위치 메모 테스팅 end
+        Route::resource(
+            '/groupPlan',
+            'GroupPlanController'
+        );
+        Route::get(
+            '/hikingPlan/{id}',
+            'HikingPlanController@getGroupPlan'
+        )->name('getGroupPlan');
+        Route::resource(
+            '/test',
+            'testcontroller'
+        );
+        Route::post(
+            '/getLocationMemoDetail',
+            'testcontroller@getLocationMemoDetail'
+        )->name('getLocationMemoDetail');
+        Route::post(
+            '/send',
+            'FCMController@pushNotification'
+        )->name('SendNotification');
+
+        Route::post(
+            '/storesend',
+            'testcontroller@updateScheduleMember'
+        )->name('updateScheduleMember');
+
+        Route::post(
+            '/getScheduleMembers',
+            'testcontroller@getScheduleMembers'
+        )->name('getScheduleMembers');
+
+        Route::post(
+            '/updateHikingState',
+            'testcontroller@updateHikingState'
+        )->name('updateHikingState');
+
+        Route::post(
+            '/getMemberNoByUserId',
+            'testcontroller@getMemberNo'
+        )->name('getMemberNoByUserId');
+
+        Route::post(
+            '/getMemberDetail',
+            'testcontroller@getMemberDetail'
+        )->name('getMemberDetail');
+
+        Route::post(
+            '/storeLocationMemo',
+            'testcontroller@storeLocationMemo'
+        )->name('storeLocationMemo');
     }
 );
 Route::group(
