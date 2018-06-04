ebb1aada7d1e3e7dd139fa8018a86a93842dc3a9
jiyoon <di03154@naver.com>
2018-06-04 11:21:18 +0900

    login change

--- resources/assets/js/components/loginAndRegister/login.vue
+++ resources/assets/js/components/loginAndRegister/login.vue
@@ -7,36 +7,36 @@
             align-content-center
             justify-center
             fluid
-            style="padding-left: 0; padding-right: 0;">
+            style="padding-left: 0; padding-right: 0; margin-left: 0px;">
         <h1
-                style="font-weight: bold;">
+                style="font-weight: bold; color: #47714c;">
             SIGN IN
         </h1>
-        <v-form>
-            <v-text-field
-                    label       ="Enter ID"
-                    v-model     ="userId"
-                    :rules      ="rules.isBlanked"
-                    prepend-icon="person"
-                    required>
-            </v-text-field>
-            <v-text-field
-                    label       ="Enter password"
-                    v-model     ="userPw"
-                    type        ="password"
-                    :rules      ="rules.isBlanked"
-                    prepend-icon="lock"
-                    required>
-            </v-text-field>
-        </v-form>
-        <v-btn
-                @click  ="login"
-                block
-                color   ="light-green"
-                dark
-                style   ="padding: 0; margin: 0 5%;">
-            sign in!
-        </v-btn>
+        <form>
+        <div class="row">
+            <div class="col-md-12">
+                <fg-input type      ="text"
+                          label     ="Enter ID"
+                          v-model   ="userId">
+                </fg-input>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <fg-input
+                          label       ="Enter password"
+                          v-model     ="userPw"
+                          type        ="password">
+                </fg-input>
+            </div>
+        </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-fill" style="background-color: #47714c; border-color: #47714c;"
+                        @click="login">
+                    sign in!
+                </button>
+            </div>
+        </form>
     </v-container>
 </template>
 
